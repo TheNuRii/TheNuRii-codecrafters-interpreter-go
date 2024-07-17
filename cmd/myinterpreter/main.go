6b415aa82e063be5d75d00f8d03247996efd037d
TheNuRii <maciekglow0@gmail.com>
2024-07-17 20:26:04 +0200

    test

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -1,54 +1,46 @@
-package main 
-
-import {
-"fms"
-"os"
-}
-
-const (
-	LEFT_PAREN rune = '('
-	RIGHT_PAREN rune = ')'
+package main
+import (
+	"fmt"
+	"os"
 )
-
+func tokenize(fileContents []byte) {
+	for _, c := range fileContents {
+		switch c {
+		case '(':
+			fmt.Println("LEFT_PAREN ( null")
+		case ')':
+			fmt.Println("RIGHT_PAREN ) null")
+		default:
+			fmt.Fprintln(os.Stderr, "Unknown character:", c)
+		}
+	}
+	// This must be the last line
+	fmt.Println("EOF  null")
+}
 func main() {
-
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
 		os.Exit(1)
 	}
-	
 	command := os.Args[1]
-
 	if command != "tokenize" {
-		fmt.Fprintln(os.Stderr, "Unkonown command %s\n", command)
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
-
+	// Uncomment this block to pass the first stage
+	//
 	filename := os.Args[2]
 	fileContents, err := os.ReadFile(filename)
-	rawfileContents, err := os.ReadFile(filename)
-	if err !=  nil {
-		fmt.Fprintln(os.Stderr, "Error reading file: %v\n", err)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
-
 	if len(fileContents) > 0 {
 		panic("Scanner not implemented")
 	} else {
-		fmt.Println("E0F null")
+		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
-
-	fileContents := string(rawfileContents)
-	
-	for _, current := range fileContents {
-		switch current {
-		case LEFT_PAREN:
-			fmt.Println("LEFT_PAREN ( null")
-
-		case: RIGHT_PAREN:
-			fmt.Println("RIGHT_PAREN ) null")
-		}
-	}
-
-	fmt.Println("E0F null")
+	tokenize(fileContents)
 }
